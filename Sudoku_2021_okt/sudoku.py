ee4c2d48e8aa88d6fa160589731e3f0a340f0b96
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-11-07 18:24:09 +0100

    comfort changing

--- Sudoku_2021_okt/sudoku.py
+++ Sudoku_2021_okt/sudoku.py
@@ -131,25 +131,26 @@ def beadott_adat_ellenőr(sudoku, sor, oszlop, szám):
     if adat != "0":
         print("A helyet már kitöltötték." )
         lehet = False
-    for o in range(1,10,1):
-        elem = sudoku[sor-1][o-1]
-        if elem == szám:
-            print("Az adott sorban már szerepel a szám")
-            lehet = False
-    for s in range(1,10,1):
-        elem = sudoku[s-1][oszlop-1]
-        if elem == szám:
-            print("Az adott oszlopban már szerepel a szám")
-            lehet = False
-    
-    sor_kezdet = (int(sor / 3.3) * 3 )
-    oszlop_kezdet = int(oszlop / 3.3) * 3
-    # print(sor_kezdet , "sor ", sor_kezdet + 3,  "   |      ",    oszlop_kezdet      ,"oszlop", oszlop_kezdet + 3)
-    for sr in range(sor_kezdet, sor_kezdet + 3, 1):
-        for os in range(oszlop_kezdet, oszlop_kezdet + 3, 1):
-            if sudoku[sr][os] == szám:
-                print("Az adott résztáblázatban már szerepel a szám")
+    else:
+        for o in range(1,10,1):
+            elem = sudoku[sor-1][o-1]
+            if elem == szám:
+                print("Az adott sorban már szerepel a szám")
                 lehet = False
+        for s in range(1,10,1):
+            elem = sudoku[s-1][oszlop-1]
+            if elem == szám:
+                print("Az adott oszlopban már szerepel a szám")
+                lehet = False
+        
+        sor_kezdet = (int(sor / 3.3) * 3 )
+        oszlop_kezdet = int(oszlop / 3.3) * 3
+        # print(sor_kezdet , "sor ", sor_kezdet + 3,  "   |      ",    oszlop_kezdet      ,"oszlop", oszlop_kezdet + 3)
+        for sr in range(sor_kezdet, sor_kezdet + 3, 1):
+            for os in range(oszlop_kezdet, oszlop_kezdet + 3, 1):
+                if sudoku[sr][os] == szám:
+                    print("Az adott résztáblázatban már szerepel a szám")
+                    lehet = False
     if lehet:
         print("A lépés megtehető")
     return lehet
@@ -172,7 +173,7 @@ def játék_interface(fájlnév):
     while ismétlés != True:
         választás = input(" [R] => Megadott adatok futtatása        [B] => visszalép a főmenübe:         [J] vagy [0] => játék ")
         if választás.capitalize() == "J" or választás.capitalize() == "0":
-            # sudoku_megjelenít(sudoku, sor, oszlop, érték, üres)
+            sudoku_megjelenít(sudoku, sor, oszlop, érték, üres)
             sor = adat_kérés("Adja meg egy sor számát!  ")
             sudoku_megjelenít(sudoku, sor, oszlop, érték, üres)
             oszlop = adat_kérés("Adja meg egy oszlop számát!  ")
@@ -184,8 +185,8 @@ def játék_interface(fájlnév):
             szám = adat_kérés("Adja meg a számot beírandó!  ")
             lehet = beadott_adat_ellenőr(sudoku, sor, oszlop, szám)
             if lehet:
-                mentsem = input(" [I] => menti:         [N] => nem menti ")
-                if mentsem.capitalize() == "I":
+                mentsem = input(" [I] vagy [0] => menti:         [N] => nem menti ")
+                if mentsem.capitalize() == "I" or mentsem == "0":
                     print("mentése:")
                     sudoku = lépés_mentés(sudoku, sor, oszlop, szám)
                     sudoku_megjelenít(sudoku, sor, oszlop, érték, üres)
